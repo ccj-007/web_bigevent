64975ca0da5bbd4572c78209d6da2fee5fc470cd
chen <596487930@qq.com>
2021-03-25 19:35:38 +0800

    完成个人功能中心的开发

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+$(function () {
+    getUserInfo()
+})
+var layer = layui.layer
+$('#btnLogout').on('click', function () {
+    layer.confirm('请确定是否退出界面', { icon: 3, title: '提示' }, function (index) {
+        //do something
+        // 清除本地存储的token
+        localStorage.removeItem('token')
+        location.href = '/home/login.html'
+        // 转到指定登录界面
+
+        layer.close(index);
+    });
+})
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: "/my/userinfo",
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data)
+        },
+        // 用此函数拿到服务器响应的数据，符合权限才能登录
+        complete: function (res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+                // 强制清空token
+                localStorage.removeItem('token')
+                // 强制跳转登录页面
+                location.href = '/home/login.html'
+            }
+        }
+
+    })
+}
+
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 1. 获取用户的名称
+    var name = user.nickname || user.username
+    // 2. 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 3.2 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,50 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称必须在1-6个字符之间'
+            }
+        }
+    })
+    initUserInfo()
+    // 初始化用户基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败')
+                }
+                // console.log(res);
+                // 为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    // 重置信息
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+    // 监听表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        //   发起数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败')
+                }
+                layer.msg('更新成功')
+                // 调用父页面的方法，重新渲染头部
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
